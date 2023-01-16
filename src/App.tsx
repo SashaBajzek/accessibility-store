233896feb1ece6d85d0fde869ce5e5abd22cb623
Sasha Bajzek <sasha.bajzek@gmail.com>
2023-01-15 21:04:15 -0800

    Consolidate cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Header from "./accessible/Header/Header";
 import "./App.css";
 
@@ -14,7 +14,7 @@ export interface CartItem {
   quantity: number;
 }
 
-const calculateQuantity = (cart: CartItem[]): number => {
+const calculateItemQuantity = (cart: CartItem[]): number => {
   const initialValue = 0;
   const sumWithInitial = cart.reduce(
     (accumulator, currentValue) => accumulator + currentValue.quantity,
@@ -27,15 +27,35 @@ function App() {
   const [cart, setCart] = React.useState<CartItem[]>([]);
   const [cartQuantity, setCartQuantity] = React.useState(0);
 
-  React.useEffect(() => {
-    setCartQuantity(calculateQuantity(cart));
-  }, [cart, setCartQuantity]);
+  const indexOfItem = useCallback(
+    (cartItem: CartItem) => {
+      if (cart.length === 0) return -1;
+      for (let i = 0; i < cart.length; i++) {
+        if (cartItem.item === cart[i].item) {
+          return i;
+        }
+      }
+      return -1;
+    },
+    [cart]
+  );
 
-  const addToCart = (cartItem: CartItem) => {
-    if (cartItem.quantity <= 0) return;
-    const newCart = [...cart, cartItem];
-    setCart(newCart);
-  };
+  const addToCart = useCallback(
+    (cartItem: CartItem) => {
+      if (cartItem.quantity <= 0) return;
+      let newCart = cart;
+      const index = indexOfItem(cartItem);
+      if (cart.length === 0 || index === -1) {
+        newCart = [...cart, cartItem];
+      } else {
+        newCart[index].quantity += cartItem.quantity;
+      }
+      console.log("cart", cart);
+      setCart(newCart);
+      setCartQuantity(calculateItemQuantity(cart));
+    },
+    [cart, indexOfItem, setCart]
+  );
 
   return (
     <div className="App">
