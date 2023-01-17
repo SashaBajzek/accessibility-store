060d16c20fc97493c5542b499b551625de205f2e
Sasha Bajzek <sasha.bajzek@gmail.com>
2023-01-16 20:12:49 -0800

    continue shopping

--- src/accessible/EmptyCartContent/EmptyCartContent.tsx
+++ src/accessible/EmptyCartContent/EmptyCartContent.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Close } from "../../SVGs/Close";
 import "./EmptyCartContent.css";
 
@@ -12,6 +13,9 @@ const EmptyCartContent = ({ closeCart }: EmptyCartContentProps) => {
         <Close />
       </button>
       <h2 id="cart-heading">Your cart is empty</h2>
+      <Link to="/" className="continue-shopping" onClick={closeCart}>
+        Continue Shopping
+      </Link>
     </div>
   );
 };
