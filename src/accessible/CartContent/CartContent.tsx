db8bf0a75bb1f724dcc47d0c5b16446abddee805
Sasha Bajzek <sasha.bajzek@gmail.com>
2023-01-16 21:01:18 -0800

    Extract CartRow

--- src/accessible/CartContent/CartContent.tsx
+++ src/accessible/CartContent/CartContent.tsx
@@ -1,9 +1,7 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
 import { CartContext } from "../../App";
 import { Close } from "../../SVGs/Close";
-import { Trash } from "../../SVGs/Trash";
-import QuantitySelect from "../QuantitySelect/QuantitySelect";
+import CartRow from "../CartRow/CartRow";
 import SubmitButton, { ButtonVariant } from "../SubmitButton/SubmitButton";
 import { VisuallyHidden } from "../VisuallyHidden/VisuallyHidden";
 import "./CartContent.css";
@@ -14,8 +12,7 @@ interface CartContentProps {
 }
 
 const CartContent = ({ checkout, closeCart }: CartContentProps) => {
-  const { cart, removeItem, totalCost, updateItemQuantity } =
-    useContext(CartContext);
+  const { cart, totalCost } = useContext(CartContext);
   return (
     <div className="CartContent">
       <div className="heading">
@@ -43,62 +40,11 @@ const CartContent = ({ checkout, closeCart }: CartContentProps) => {
           </thead>
           <tbody>
             {cart.map((cartItem) => (
-              <tr key={cartItem.item.id + cartItem.size}>
-                <td className="product-col">
-                  <Link
-                    aria-hidden={true}
-                    to={"items/" + cartItem.item.id}
-                    onClick={closeCart}
-                    tabIndex={-1}
-                  >
-                    {cartItem.item.images ? (
-                      <img
-                        alt=""
-                        aria-hidden={true}
-                        className="image"
-                        src={`${process.env.PUBLIC_URL}/assets/${cartItem.item.images[0].fileName}`}
-                      ></img>
-                    ) : (
-                      <div>LoadingImage</div>
-                    )}
-                  </Link>
-                  <div className="product-text">
-                    <Link to={"items/" + cartItem.item.id} onClick={closeCart}>
-                      <div className="item-name">{cartItem.item.name}</div>
-                    </Link>
-                    <div>Length: {cartItem.size}</div>
-                  </div>
-                </td>
-                <td>
-                  <div>${cartItem.item.price}</div>
-                </td>
-                <td>
-                  <div className="quantity-container">
-                    <QuantitySelect
-                      maxQuantity={Math.max(cartItem.quantity, 10)}
-                      minQuantity={0}
-                      onChange={(newQuantity: number) => {
-                        updateItemQuantity({
-                          item: cartItem.item,
-                          quantity: newQuantity,
-                          size: cartItem.size,
-                        });
-                      }}
-                      quantity={cartItem.quantity}
-                    />
-                  </div>
-                </td>
-                <td>${cartItem.item.price * cartItem.quantity}</td>
-                <td>
-                  <button
-                    aria-label={`Remove ${cartItem.item.name} braceletfrom cart`}
-                    className="remove-button"
-                    onClick={() => removeItem(cartItem)}
-                  >
-                    <Trash />
-                  </button>
-                </td>
-              </tr>
+              <CartRow
+                cartItem={cartItem}
+                closeCart={closeCart}
+                key={cartItem.item.id + cartItem.size}
+              />
             ))}
           </tbody>
         </table>
---
+++ src/accessible/CartRow/CartRow.tsx
@@ -0,0 +1,74 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { CartContext } from "../../App";
+import { CartItem } from "../../cartUtils";
+import { Trash } from "../../SVGs/Trash";
+import QuantitySelect from "../QuantitySelect/QuantitySelect";
+import "./CartRow.css";
+
+interface CartRowProps {
+  cartItem: CartItem;
+  closeCart: () => void;
+}
+
+const CartRow = ({ cartItem, closeCart }: CartRowProps) => {
+  const { removeItem, updateItemQuantity } = useContext(CartContext);
+  return (
+    <tr className="CartRow">
+      <td className="product-col">
+        <Link
+          aria-hidden={true}
+          to={"items/" + cartItem.item.id}
+          onClick={closeCart}
+          tabIndex={-1}
+        >
+          {cartItem.item.images ? (
+            <img
+              alt=""
+              aria-hidden={true}
+              className="image"
+              src={`${process.env.PUBLIC_URL}/assets/${cartItem.item.images[0].fileName}`}
+            ></img>
+          ) : (
+            <div>LoadingImage</div>
+          )}
+        </Link>
+        <div className="product-text">
+          <Link to={"items/" + cartItem.item.id} onClick={closeCart}>
+            <div className="item-name">{cartItem.item.name}</div>
+          </Link>
+          <div>Length: {cartItem.size}</div>
+        </div>
+      </td>
+      <td>
+        <div>${cartItem.item.price}</div>
+      </td>
+      <td>
+        <QuantitySelect
+          maxQuantity={Math.max(cartItem.quantity, 10)}
+          minQuantity={0}
+          onChange={(newQuantity: number) => {
+            updateItemQuantity({
+              item: cartItem.item,
+              quantity: newQuantity,
+              size: cartItem.size,
+            });
+          }}
+          quantity={cartItem.quantity}
+        />
+      </td>
+      <td>${cartItem.item.price * cartItem.quantity}</td>
+      <td>
+        <button
+          aria-label={`Remove ${cartItem.item.name} braceletfrom cart`}
+          className="remove-button"
+          onClick={() => removeItem(cartItem)}
+        >
+          <Trash />
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default CartRow;
