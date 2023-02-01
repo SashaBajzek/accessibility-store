61de3bce3ff92a7ebcf8057daf0b8caf60b1aa1d
Sasha Bajzek <sasha.bajzek@gmail.com>
2023-01-31 21:03:37 -0800

    Footer role contentinfo

--- src/accessible/Footer/Footer.tsx
+++ src/accessible/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="Footer">
+    <footer className="Footer" role="contentinfo">
       <small className="copyright">
         &copy; <span id="year"></span> Sasha Bajzek
       </small>
