9cc158cba2481e58043d3e7a429ffcd14e0919c7
Sasha Bajzek <sasha.bajzek@gmail.com>
2023-02-01 21:02:07 -0800

    remove extra region

--- src/accessible/ImageGallery/ImageGallery.tsx
+++ src/accessible/ImageGallery/ImageGallery.tsx
@@ -7,18 +7,14 @@ interface ImageGallerytProps {
 
 const ImageGallery = ({ images }: ImageGallerytProps) => {
   return (
-    <ul
-      aria-label="Product Image Gallery"
-      className="ImageGallery"
-      role="region"
-    >
+    <ul className="ImageGallery">
       {images.map((image) => (
         <li className="listItem" key={image.fileName}>
           <img
             alt={image.description}
             className="image"
             src={`${process.env.PUBLIC_URL}/assets/${image.fileName}`}
-        />
+          />
         </li>
       ))}
     </ul>
