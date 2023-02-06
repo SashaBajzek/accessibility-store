2437ce8818fc362c12149377f6c15101cd7e6acd
Sasha Bajzek <sasha.bajzek@gmail.com>
2023-02-05 18:33:39 -0800

    Add test

---
+++ src/accessible/QuantityPicker/QuantityPicker.test.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import QuantityPicker from "./QuantityPicker";
+
+describe("QuantityPicker", () => {
+  test("Increments quantity", () => {
+    const Wrapper = () => {
+      const [quantity, setQuantity] = useState(1);
+      return (
+        <QuantityPicker
+          error=""
+          itemName="Item"
+          maxQuantity={10}
+          minQuantity={0}
+          quantity={quantity}
+          setQuantity={setQuantity}
+        />
+      );
+    };
+    render(<Wrapper />);
+    const incrementButton = screen.getByLabelText("Increment quantity of Item");
+    expect(incrementButton).toBeInTheDocument();
+    fireEvent.click(incrementButton);
+    const input = screen.getByRole("spinbutton");
+    expect(input.getAttribute("value")).toBe("2");
+  });
+});
