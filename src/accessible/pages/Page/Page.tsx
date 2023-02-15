8dcd8142968f8aa0dc61cea167ed3a34c912a576
Sasha Bajzek <sasha.bajzek@gmail.com>
2023-02-14 18:31:55 -0800

    Focus heading on render

--- src/accessible/pages/Page/Page.tsx
+++ src/accessible/pages/Page/Page.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useContext, useEffect } from "react";
+import { ReactNode, useContext, useEffect, useRef } from "react";
 
 import "./Page.css";
 import { SetTitleContext } from "../../../context/SetTitleContext";
@@ -11,14 +11,21 @@ interface PageProps {
 }
 
 const Page = ({ children, className, heading, title }: PageProps) => {
+  const headingRef = useRef<HTMLHeadingElement>(null);
   const { setTitle } = useContext(SetTitleContext);
   useEffect(() => {
     setTitle(title);
   }, [setTitle, title]);
 
+  useEffect(() => {
+    headingRef.current?.focus();
+  }, []);
+
   return (
     <main className={`Page ${className || ""}`}>
-      <h1>{heading}</h1>
+      <h1 ref={headingRef} tabIndex={-1}>
+        {heading}
+      </h1>
       {children}
     </main>
   );
